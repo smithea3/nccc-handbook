cfd02493d8672af0032e5df776f2d7e2875d0134
Ethan A. Smith <smithea3@outlook.com>
2023-11-10 18:54:13 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "The Basics of Full-time Equivalence",
-  "body": " The Basics of Full-time Equivalence  The term full-time equivalence is not endemic to colleges and universities. Many human resources departments at other companies and organizations use the term full-time equivalence when determining business aspects. For example, a company may wish to calculate the profits per employee. To do so, they must consider the full-time employees and the part-time employees time into an equivalent measure for comparison and calculation. In mathematics, the process of calculating a singluar standard by which to compare individuals within a group is called normalizing .  As stated in the introduction of this chapter, the term FTE is used in other institutions and organizations other than just colleges and university. Therefore, the following definitions are necessary to establish the calculations that will be discussed later.   Census Date  The [North Carolina] State Voard of Community College Code 1G SBCCC 200.93: Reporting of Student Hours in Membership for Curriculum Classes states that the census date is the date at which 10% of the course's meeting time has occured.    Number of Students  The number of students is the number of students participating in a course at the of the course.    Student Membership Hours  The student membership hours is the total number of hours each student meets in a course.    Course Membership Hours  The course membership hours is the total number of in a course.  To calculate the course members hours, the formula for this calculation is   Notice that course membership hours, regardless of the number of weeks that a course meets, is calculated using a 16-week baseline.   Consider the following example to give context to these definition.    Consider an ENG-111: Writing and Inquiry course that originally had 24 students on the roster at the start date of the course. Now consider, as you may have witnessed, that two students drop the course before the , and another student never participated in any part of the course by the Census Date. This means that 21 students are participating in the course at the Census Date; therefore, the in the course would be 21.  The for ENG-111, the calculati    "
+  "body": " The Basics of Full-time Equivalence  The term full-time equivalence is not specific to colleges and universities. Many human resources departments at other companies and organizations use the term full-time equivalence when determining business aspects. For example, a company may wish to calculate the profits per employee. To do so, they must consider the full-time employees and the part-time employees time into an equivalent measure for comparison and calculation. In mathematics, the process of calculating a standard by which to compare individuals within a group is a process called normalization .   Definitions for NCCCS FTE Calculation  As stated in the introduction of this chapter, the term FTE is used in other institutions and organizations other than just colleges and university. Therefore, the following definitions are necessary to establish the calculations specific to college and universities.   Census Date  The [North Carolina] State Voard of Community College Code 1G SBCCC 200.93: Reporting of Student Hours in Membership for Curriculum Classes states that the census date is the date at which 10% of the course's meeting time has occured.    Number of Students  The number of students is the number of students participating in a course at the of the course.    Student Membership Hours  The student membership hours is the total number of hours each student meets in a course.  To calculate the student membership hours (SMH), the formula is   Notice that student membership hours, regardless of the number of weeks that a course meets, is calculated using a 16-week baseline.    Course Membership Hours  The course membership hours is the total number of in a course.  To calculate the course membership hours, the formula is    Consider the following example to give context to these definition.    Consider an ENG-111: Writing and Inquiry course that originally had 24 students on the roster at the start date of the course. Now consider, as you may have witnessed, that two students drop the course before the , and another student never participated in any part of the course by the Census Date. This means that 21 students are participating in the course at the Census Date; therefore, the in the course would be 21.  For ENG-111, the number of contact hours is set at three hours. Therefore, the (CMH) for ENG-111, is calclated to be   Furthermore, the course membership hours would be       Calculating FTE in the NCCCS   Be advised that the numbers for FTE used in this part of this section are subject to change. Specifically, the denominator value discussed below may change with new laws, policies, and procedures created at the state level. While the best effort is made to ensure that the numbers used in this text is accurate, the informationw written herein represents the latest numbers as of .    The FTE Denominator   Again, when considering the calculations for FTE, it is important to have a metric by which to compare a course's enrollment on a full-time basis (hence the term full-time equivalence). A value that normalizes the course membership hours is needed to compare the various courses enrollment contribution to an institution.  As of the number that is used to normalize a course's enrollment contribution is 512. To get this value, the NCCCS considers the average full-time student to have 16 contact hours of courses, multiplied by 16 weeks in a semester, multiplied by 2 semesters in an academic year.     The NCCCS FTE Calculation   To calculate the FTE of a course in the NCCCS, the course membership hours is divided by 512 (see ). The formula for this calculation is     Revisit the numbers from for the following example.   To calculate the FTE of the ENG-111 course from , the following calcualtion would be performed.     "
 },
 {
   "id": "sec-the-basics-of-fte-2",
@@ -25,7 +25,7 @@ var ptx_lunr_docs = [
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
-  "body": "normalizing "
+  "body": "normalization "
 },
 {
   "id": "def-census-date",
@@ -52,7 +52,7 @@ var ptx_lunr_docs = [
   "type": "Definition",
   "number": "1.1.3",
   "title": "Student Membership Hours.",
-  "body": " Student Membership Hours  The student membership hours is the total number of hours each student meets in a course.  "
+  "body": " Student Membership Hours  The student membership hours is the total number of hours each student meets in a course.  To calculate the student membership hours (SMH), the formula is   Notice that student membership hours, regardless of the number of weeks that a course meets, is calculated using a 16-week baseline.  "
 },
 {
   "id": "def-course-membership-hours",
@@ -61,16 +61,52 @@ var ptx_lunr_docs = [
   "type": "Definition",
   "number": "1.1.4",
   "title": "Course Membership Hours.",
-  "body": " Course Membership Hours  The course membership hours is the total number of in a course.  To calculate the course members hours, the formula for this calculation is   Notice that course membership hours, regardless of the number of weeks that a course meets, is calculated using a 16-week baseline.  "
+  "body": " Course Membership Hours  The course membership hours is the total number of in a course.  To calculate the course membership hours, the formula is   "
 },
 {
-  "id": "sec-the-basics-of-fte-9",
+  "id": "example-eng-111-with-21-students",
   "level": "2",
-  "url": "sec-the-basics-of-fte.html#sec-the-basics-of-fte-9",
+  "url": "sec-the-basics-of-fte.html#example-eng-111-with-21-students",
   "type": "Example",
   "number": "1.1.5",
   "title": "",
-  "body": "  Consider an ENG-111: Writing and Inquiry course that originally had 24 students on the roster at the start date of the course. Now consider, as you may have witnessed, that two students drop the course before the , and another student never participated in any part of the course by the Census Date. This means that 21 students are participating in the course at the Census Date; therefore, the in the course would be 21.  The for ENG-111, the calculati   "
+  "body": "  Consider an ENG-111: Writing and Inquiry course that originally had 24 students on the roster at the start date of the course. Now consider, as you may have witnessed, that two students drop the course before the , and another student never participated in any part of the course by the Census Date. This means that 21 students are participating in the course at the Census Date; therefore, the in the course would be 21.  For ENG-111, the number of contact hours is set at three hours. Therefore, the (CMH) for ENG-111, is calclated to be   Furthermore, the course membership hours would be    "
+},
+{
+  "id": "subsec-calculating-fte-2",
+  "level": "2",
+  "url": "sec-the-basics-of-fte.html#subsec-calculating-fte-2",
+  "type": "Warning",
+  "number": "1.1.6",
+  "title": "",
+  "body": " Be advised that the numbers for FTE used in this part of this section are subject to change. Specifically, the denominator value discussed below may change with new laws, policies, and procedures created at the state level. While the best effort is made to ensure that the numbers used in this text is accurate, the informationw written herein represents the latest numbers as of .  "
+},
+{
+  "id": "fact-fte-denominator",
+  "level": "2",
+  "url": "sec-the-basics-of-fte.html#fact-fte-denominator",
+  "type": "Fact",
+  "number": "1.1.7",
+  "title": "The FTE Denominator.",
+  "body": " The FTE Denominator   Again, when considering the calculations for FTE, it is important to have a metric by which to compare a course's enrollment on a full-time basis (hence the term full-time equivalence). A value that normalizes the course membership hours is needed to compare the various courses enrollment contribution to an institution.  As of the number that is used to normalize a course's enrollment contribution is 512. To get this value, the NCCCS considers the average full-time student to have 16 contact hours of courses, multiplied by 16 weeks in a semester, multiplied by 2 semesters in an academic year.   "
+},
+{
+  "id": "def-fte-calculation",
+  "level": "2",
+  "url": "sec-the-basics-of-fte.html#def-fte-calculation",
+  "type": "Definition",
+  "number": "1.1.8",
+  "title": "The NCCCS FTE Calculation.",
+  "body": " The NCCCS FTE Calculation   To calculate the FTE of a course in the NCCCS, the course membership hours is divided by 512 (see ). The formula for this calculation is    "
+},
+{
+  "id": "subsec-calculating-fte-6",
+  "level": "2",
+  "url": "sec-the-basics-of-fte.html#subsec-calculating-fte-6",
+  "type": "Example",
+  "number": "1.1.9",
+  "title": "",
+  "body": " To calculate the FTE of the ENG-111 course from , the following calcualtion would be performed.   "
 },
 {
   "id": "backmatter-2",
