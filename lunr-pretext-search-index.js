f839c099b347534ede62560b26c8c401df7d74d7
Ethan A. Smith <smithea3@outlook.com>
2023-11-13 15:28:28 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
-  "body": "  My Website   copyright  "
+  "body": "  Visit my GitHub repositories.   copyright  "
 },
 {
   "id": "sec-the-basics-of-fte",
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "The Basics of Full-time Equivalence",
-  "body": " The Basics of Full-time Equivalence  The term full-time equivalence is not specific to colleges and universities. Many human resources departments at other companies and organizations use the term full-time equivalence when determining business aspects. For example, a company may wish to calculate the profits per employee. To do so, they must consider the full-time employees and the part-time employees time into an equivalent measure for comparison and calculation. In mathematics, the process of calculating a standard by which to compare individuals within a group is a process called normalization .   Definitions for NCCCS FTE Calculation  As stated in the introduction of this chapter, the term FTE is used in other institutions and organizations other than just colleges and university. Therefore, the following definitions are necessary to establish the calculations specific to college and universities.   Census Date  The [North Carolina] State Voard of Community College Code 1G SBCCC 200.93: Reporting of Student Hours in Membership for Curriculum Classes states that the census date is the date at which 10% of the course's meeting time has occured.    Number of Students  The number of students is the number of students participating in a course at the of the course.    Student Membership Hours  The student membership hours is the total number of hours each student meets in a course.  To calculate the student membership hours (SMH), the formula is   Notice that student membership hours, regardless of the number of weeks that a course meets, is calculated using a 16-week baseline.    Course Membership Hours  The course membership hours is the total number of in a course.  To calculate the course membership hours, the formula is    Consider the following example to give context to these definition.    Consider an ENG-111: Writing and Inquiry course that originally had 24 students on the roster at the start date of the course. Now consider, as you may have witnessed, that two students drop the course before the , and another student never participated in any part of the course by the Census Date. This means that 21 students are participating in the course at the Census Date; therefore, the in the course would be 21.  For ENG-111, the number of contact hours is set at three hours. Therefore, the (CMH) for ENG-111, is calclated to be   Furthermore, the course membership hours would be       Calculating FTE in the NCCCS   Be advised that the numbers for FTE used in this part of this section are subject to change. Specifically, the denominator value discussed below may change with new laws, policies, and procedures created at the state level. While the best effort is made to ensure that the numbers used in this text is accurate, the informationw written herein represents the latest numbers as of .    The FTE Denominator   Again, when considering the calculations for FTE, it is important to have a metric by which to compare a course's enrollment on a full-time basis (hence the term full-time equivalence). A value that normalizes the course membership hours is needed to compare the various courses enrollment contribution to an institution.  As of the number that is used to normalize a course's enrollment contribution is 512. To get this value, the NCCCS considers the average full-time student to have 16 contact hours of courses, multiplied by 16 weeks in a semester, multiplied by 2 semesters in an academic year.     The NCCCS FTE Calculation   To calculate the FTE of a course in the NCCCS, the course membership hours is divided by 512 (see ). The formula for this calculation is     Revisit the numbers from for the following example.   To calculate the FTE of the ENG-111 course from , the following calcualtion would be performed.     "
+  "body": " The Basics of Full-time Equivalence  The term full-time equivalence is not specific to colleges and universities. Many human resources departments at other companies and organizations use the term full-time equivalence when determining business aspects. For example, a company may wish to calculate the profits per employee. To do so, they must consider the full-time employees and the part-time employees time into an equivalent measure for comparison and calculation. In mathematics, the process of calculating a standard by which to compare individuals within a group is a process called normalization .   Definitions for NCCCS FTE Calculation  As stated in the introduction of this chapter, the term FTE is used in other institutions and organizations other than just colleges and university. Therefore, the following definitions are necessary to establish the calculations specific to college and universities.   Contact Hours   The number of contact hours of a course is the actual time of student attendance in a class or lab. For records purposes, 60 minutes constitutes an hour.      Consider the Nurse Aide I (NAS-101) in the NCCCS Combined Course Library. This course has three class hours, four lab hours, and three clinical hours. This would sum to be 10 contact hours for the course.     Credit Hours  The number of contact hours of a course is different from the credit hours . The number of credit hours is calculated based on a weighted average of the four designations for meeting times (i.e., class, lab, clinical, and work). The weights for the meeting time designations are 1, 0.5, and 0.10 respectively.     For example, the Nurse Aide I (NAS-101) course would have six credit hours based on the weighted calculation as follows:      Census Date  The [North Carolina] State Voard of Community College Code 1G SBCCC 200.93: Reporting of Student Hours in Membership for Curriculum Classes states that the census date is the date at which 10% of the course's meeting time has occured.    Number of Students  The number of students is the number of students participating in a course at the of the course.    Student Membership Hours  The student membership hours is the total number of contact hours each student meets in a course.  To calculate the student membership hours (SMH), the formula is   Notice that student membership hours, regardless of the number of weeks that a course meets, is calculated using a 16-week baseline.    Course Membership Hours  The course membership hours is the total number of in a course.  To calculate the course membership hours, the formula is    Consider the following example to give context to these definitions.    Consider an ENG-111: Writing and Inquiry course that originally had 24 students on the roster at the start date of the course. Now consider, as you may have witnessed, that two students drop the course before the , and another student never participated in any part of the course by the Census Date. This means that 21 students are participating in the course at the Census Date; therefore, the in the course would be 21.  For ENG-111, the number of contact hours is set at three hours. Therefore, the (CMH) for ENG-111, is calclated to be   Furthermore, the course membership hours would be       Calculating FTE in the NCCCS   Be advised that the numbers for FTE used in this part of this section are subject to change. Specifically, the denominator value discussed below may change with new laws, policies, and procedures created at the state level. While the best effort is made to ensure that the numbers used in this text is accurate, the informationw written herein represents the latest numbers as of .    The FTE Denominator   Again, when considering the calculations for FTE, it is important to have a metric by which to compare a course's enrollment on a full-time basis (hence the term full-time equivalence). A value that normalizes the course membership hours is needed to compare the various courses enrollment contribution to an institution.  As of the number that is used to normalize a course's enrollment contribution is 512. To get this value, the NCCCS considers the average full-time student to have 16 contact hours of courses, multiplied by 16 weeks in a semester, multiplied by 2 semesters in an academic year.     The NCCCS FTE Calculation   To calculate the FTE of a course in the NCCCS, the course membership hours is divided by 512 (see ). The formula for this calculation is     Revisit the numbers from for the following example.   To calculate the FTE of the ENG-111 course from , the following calcualtion would be performed.     "
 },
 {
   "id": "sec-the-basics-of-fte-2",
@@ -28,11 +28,47 @@ var ptx_lunr_docs = [
   "body": "normalization "
 },
 {
+  "id": "def-contact-hours",
+  "level": "2",
+  "url": "sec-the-basics-of-fte.html#def-contact-hours",
+  "type": "Definition",
+  "number": "1.1.1",
+  "title": "Contact Hours.",
+  "body": " Contact Hours   The number of contact hours of a course is the actual time of student attendance in a class or lab. For records purposes, 60 minutes constitutes an hour.   "
+},
+{
+  "id": "subsec-definitions-4",
+  "level": "2",
+  "url": "sec-the-basics-of-fte.html#subsec-definitions-4",
+  "type": "Example",
+  "number": "1.1.2",
+  "title": "",
+  "body": "  Consider the Nurse Aide I (NAS-101) in the NCCCS Combined Course Library. This course has three class hours, four lab hours, and three clinical hours. This would sum to be 10 contact hours for the course.   "
+},
+{
+  "id": "def-credit-hours",
+  "level": "2",
+  "url": "sec-the-basics-of-fte.html#def-credit-hours",
+  "type": "Definition",
+  "number": "1.1.3",
+  "title": "Credit Hours.",
+  "body": " Credit Hours  The number of contact hours of a course is different from the credit hours . The number of credit hours is calculated based on a weighted average of the four designations for meeting times (i.e., class, lab, clinical, and work). The weights for the meeting time designations are 1, 0.5, and 0.10 respectively.  "
+},
+{
+  "id": "subsec-definitions-6",
+  "level": "2",
+  "url": "sec-the-basics-of-fte.html#subsec-definitions-6",
+  "type": "Example",
+  "number": "1.1.4",
+  "title": "",
+  "body": "  For example, the Nurse Aide I (NAS-101) course would have six credit hours based on the weighted calculation as follows:    "
+},
+{
   "id": "def-census-date",
   "level": "2",
   "url": "sec-the-basics-of-fte.html#def-census-date",
   "type": "Definition",
-  "number": "1.1.1",
+  "number": "1.1.5",
   "title": "Census Date.",
   "body": " Census Date  The [North Carolina] State Voard of Community College Code 1G SBCCC 200.93: Reporting of Student Hours in Membership for Curriculum Classes states that the census date is the date at which 10% of the course's meeting time has occured.  "
 },
@@ -41,7 +77,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-the-basics-of-fte.html#def-number-of-students",
   "type": "Definition",
-  "number": "1.1.2",
+  "number": "1.1.6",
   "title": "Number of Students.",
   "body": " Number of Students  The number of students is the number of students participating in a course at the of the course.  "
 },
@@ -50,16 +86,16 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-the-basics-of-fte.html#def-student-membership-hours",
   "type": "Definition",
-  "number": "1.1.3",
+  "number": "1.1.7",
   "title": "Student Membership Hours.",
-  "body": " Student Membership Hours  The student membership hours is the total number of hours each student meets in a course.  To calculate the student membership hours (SMH), the formula is   Notice that student membership hours, regardless of the number of weeks that a course meets, is calculated using a 16-week baseline.  "
+  "body": " Student Membership Hours  The student membership hours is the total number of contact hours each student meets in a course.  To calculate the student membership hours (SMH), the formula is   Notice that student membership hours, regardless of the number of weeks that a course meets, is calculated using a 16-week baseline.  "
 },
 {
   "id": "def-course-membership-hours",
   "level": "2",
   "url": "sec-the-basics-of-fte.html#def-course-membership-hours",
   "type": "Definition",
-  "number": "1.1.4",
+  "number": "1.1.8",
   "title": "Course Membership Hours.",
   "body": " Course Membership Hours  The course membership hours is the total number of in a course.  To calculate the course membership hours, the formula is   "
 },
@@ -68,7 +104,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-the-basics-of-fte.html#example-eng-111-with-21-students",
   "type": "Example",
-  "number": "1.1.5",
+  "number": "1.1.9",
   "title": "",
   "body": "  Consider an ENG-111: Writing and Inquiry course that originally had 24 students on the roster at the start date of the course. Now consider, as you may have witnessed, that two students drop the course before the , and another student never participated in any part of the course by the Census Date. This means that 21 students are participating in the course at the Census Date; therefore, the in the course would be 21.  For ENG-111, the number of contact hours is set at three hours. Therefore, the (CMH) for ENG-111, is calclated to be   Furthermore, the course membership hours would be    "
 },
@@ -77,7 +113,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-the-basics-of-fte.html#subsec-calculating-fte-2",
   "type": "Warning",
-  "number": "1.1.6",
+  "number": "1.1.10",
   "title": "",
   "body": " Be advised that the numbers for FTE used in this part of this section are subject to change. Specifically, the denominator value discussed below may change with new laws, policies, and procedures created at the state level. While the best effort is made to ensure that the numbers used in this text is accurate, the informationw written herein represents the latest numbers as of .  "
 },
@@ -86,7 +122,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-the-basics-of-fte.html#fact-fte-denominator",
   "type": "Fact",
-  "number": "1.1.7",
+  "number": "1.1.11",
   "title": "The FTE Denominator.",
   "body": " The FTE Denominator   Again, when considering the calculations for FTE, it is important to have a metric by which to compare a course's enrollment on a full-time basis (hence the term full-time equivalence). A value that normalizes the course membership hours is needed to compare the various courses enrollment contribution to an institution.  As of the number that is used to normalize a course's enrollment contribution is 512. To get this value, the NCCCS considers the average full-time student to have 16 contact hours of courses, multiplied by 16 weeks in a semester, multiplied by 2 semesters in an academic year.   "
 },
@@ -95,7 +131,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-the-basics-of-fte.html#def-fte-calculation",
   "type": "Definition",
-  "number": "1.1.8",
+  "number": "1.1.12",
   "title": "The NCCCS FTE Calculation.",
   "body": " The NCCCS FTE Calculation   To calculate the FTE of a course in the NCCCS, the course membership hours is divided by 512 (see ). The formula for this calculation is    "
 },
@@ -104,9 +140,27 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-the-basics-of-fte.html#subsec-calculating-fte-6",
   "type": "Example",
-  "number": "1.1.9",
+  "number": "1.1.13",
   "title": "",
   "body": " To calculate the FTE of the ENG-111 course from , the following calcualtion would be performed.   "
+},
+{
+  "id": "sec-fte-and-college-funding.ptx",
+  "level": "1",
+  "url": "sec-fte-and-college-funding.ptx.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Full-time Equivalence and College Funding",
+  "body": " Full-time Equivalence and College Funding   The previous section established the definitions and calcualtions for full-time equivalence. In this section, the impact that FTE has on college-level funding and how funds are allocated based on FTE is discussed. When reading this informaiton, it is important to keep in mind that the information and numbers represent the information available at the time of writing. Legislation may impact the values of the calculations between times when this document is updated.    Tiered Funding Based on FTE  Each course in the North Carolina Community College System has various and expectations for time needed for instruction are accounted for in the of the course.   Tier Funding for FTE   Certain courses in the NCCCS are associated with higher costs. This higher cost is adjusted in the college's allocated funds based on a tiered system of funding. The NCCCS uses a four-tier system. The following are the four tiers with their respective funding level Click here to see the Fiscal Research Division of the NC General Assembly presentation on these numbers. .   Tier 1A ($4,889.65): This is the highest level of funding tier. Courses in this tier include health care and technical education courses.    Tier 1B ($4,325.46): Courses in this tier include lab-based science courses, college-level mathematics courses, and some courses in health care and technical education not funded at the Tier 1A level.    Tier 2 ($3,721.27): This tier includes all other curriculum courses, basic skills, and other continuing education courses scheduled for more than 96 or more contact hours leading to a credential, certification or indurstry-designed curriculum.    Tier 3 ($2,380.88): This tier is for all other continuing education courses.        "
+},
+{
+  "id": "fact-tier-funding-for-fte",
+  "level": "2",
+  "url": "sec-fte-and-college-funding.ptx.html#fact-tier-funding-for-fte",
+  "type": "Fact",
+  "number": "1.2.1",
+  "title": "Tier Funding for FTE.",
+  "body": " Tier Funding for FTE   Certain courses in the NCCCS are associated with higher costs. This higher cost is adjusted in the college's allocated funds based on a tiered system of funding. The NCCCS uses a four-tier system. The following are the four tiers with their respective funding level Click here to see the Fiscal Research Division of the NC General Assembly presentation on these numbers. .   Tier 1A ($4,889.65): This is the highest level of funding tier. Courses in this tier include health care and technical education courses.    Tier 1B ($4,325.46): Courses in this tier include lab-based science courses, college-level mathematics courses, and some courses in health care and technical education not funded at the Tier 1A level.    Tier 2 ($3,721.27): This tier includes all other curriculum courses, basic skills, and other continuing education courses scheduled for more than 96 or more contact hours leading to a credential, certification or indurstry-designed curriculum.    Tier 3 ($2,380.88): This tier is for all other continuing education courses.      "
 },
 {
   "id": "backmatter-2",
